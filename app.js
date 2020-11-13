758c4b2e54ddca0eb4a90b83936852e4b4e31d03
f0rGetFull <f0rgetful@yandex.ru>
2020-11-14 00:04:13 +0300

    add reqvest body

--- app.js
+++ app.js
@@ -1,44 +1,94 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
-const {userScheme} = require('./model/sheme')
-let passport = require('passport')
-let LocalStrategy = require('passport-local').Strategy;
+const MySheme = require('./model/sheme')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + "/public"));
+app.use(passport.initialize());
+
+const UserDB = {
+  username:"user1",
+  password:"pass1"
+}
+
+
+passport.use(new LocalStrategy({ },
+    function(username, password, done) {
+        console.log('strat')
+        const user = UserDB;
+        if(user.username == username && user.password == password){
+            return done(null, user);
+        }else{
+            console.log('nevern') 
+            return done(null, false);
+        }
+    }
+
+  ));
+
+const User = mongoose.model('user', MySheme);
+
+  
+// app.post('/', 
+//   passport.authenticate('local', { failureRedirect: '/' ,session: false}),
+//   function(req, res) {
+//     res.redirect('/app');
+//   });
 
 
 
 
-require('./routes')(app)
+
+app.use('/mass',async (req,res)=>{
+  console.log('mass')
+  let test;
+  let dat = await User.find({},(err,doc)=>{
+    test = doc
+  });
+  res.json(test)
+})
+
+  // app.use('/app',  passport.authenticate('local', { failureRedirect: '/' ,session: false}),
+  //   (req,res)=>{
+  //   console.log('app')
+  //   res.sendFile(__dirname + '/public/app.html')
+  // })
+
+    app.use('/app', 
+    (req,res)=>{
+    console.log('app')
+    res.sendFile(__dirname + '/public/app.html')
+  })
+
+  app.post('/bd',(req,res)=>{
+    console.log('bd')
+    console.log(req.body)
+    })
+
+  app.use('/',(req,res)=>{
+    console.log('/')
+    res.sendFile(__dirname + '/public/login.html')
+    })
 
 
-
-const User = mongoose.model("User", userScheme);
-const user = new User({
-    value: "sgtring string",
-    work:1,
-    work2:12,
-    list:4,
-    good:true,
-    data:"data string"
-});
-
-
-// user.save(function(err){    
-//     if(err) return console.log(err);
-//     console.log("Сохранен объект", user);
-// });
 
 
 
 async function start (){
-try{
-    mongoose.connect("mongodb+srv://dataBase:1q2w3e@cluster0.ejvhd.mongodb.net/collect1", { useNewUrlParser: true ,useUnifiedTopology: true},()=>{
-    app.listen(3000)
-});
-}catch(e){
-    console.log(e)
-}
-}start()
+  try{
+      mongoose.connect("mongodb+srv://dataBase:1q2w3e@cluster0.ejvhd.mongodb.net/collect1", { useNewUrlParser: true ,useUnifiedTopology: true},()=>{
+      app.listen(3000,()=>{
+          console.log('server is done')
+      })
+  });
+  }catch(e){
+      console.log(e)
+  }
+  }start()
 
---
+++ public/script.js
@@ -0,0 +1,66 @@
+
+
+let cont = document.getElementById('cont');
+let btn = document.getElementById('btn')
+let form = document.getElementById('form')
+
+async function start(){
+console.log('start is work')
+let url = '/mass'
+let response = await fetch(url);
+
+if (await response.ok) { 
+  let json = await response.json();
+  console.log(json);
+  if(json.length){
+      for(let i = 0; i < json.length;i++){
+          create(json[i])
+      }
+  }
+} else {
+  alert("Ошибка HTTP: " + response.status);
+}
+}start()
+
+
+async function meFetch(data){
+  let response = await fetch('/bd',{
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json;charset=UTF-8'
+    },
+    body: data
+  });
+}
+
+function create(elem){
+    let div = document.createElement('div');
+    let good = document.createElement('p')
+    let list = document.createElement('p')
+    let value = document.createElement('p')
+    let work = document.createElement('p')
+    let work2 = document.createElement('p')
+    div.className = "card blue-grey darken-1 "
+    good.innerText = elem.good
+    list.innerText = elem.list
+    value.innerText = elem.value
+    work.innerText = elem.work
+    work2.innerText = elem.work2
+    cont.append(div);
+    div.append(good,list,value,work,work2)
+    
+}
+
+btn.addEventListener('click',push)
+
+
+
+function push(){
+  event.preventDefault();
+  let fd = new FormData(form);
+  console.log(fd.get('value'))
+  let data = {"value": fd.get('value'),"data":fd.get('data'),"work": fd.get('work'),"work2":fd.get('work2'),"work": fd.get('work'),"good":fd.get('good'),"work": fd.get('work'),"list":fd.get('list')}
+  
+  meFetch(JSON.stringify(data))
+  
+}
